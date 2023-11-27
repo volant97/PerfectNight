528f4ba86fe26d6e5dcb79818bd8e6b816023ce8
volant97 <volant97@naver.com>
2023-11-27 21:14:53 +0900

    배포테스트

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -1,12 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 import Home from "pages/Home";
 import Letter from "pages/Letter";
 
 const Router = (props) => {
-  const { artistData, dummyData } = props
+  const { artistData, dummyData } = props;
 
-  const [letters, setLetters] = useState(dummyData)
+  const [letters, setLetters] = useState(dummyData);
 
   const [selected, setSelected] = useState({
     허윤진: false,
@@ -14,31 +14,36 @@ const Router = (props) => {
     김채원: true,
     카즈하: false,
     홍은채: false,
-  })
-  const [btnClicked, setBtnClicked] = useState("김채원")
+  });
+  const [btnClicked, setBtnClicked] = useState("김채원");
   const [writedTo, setWritedTo] = useState(btnClicked);
 
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home
-          artistData={artistData}
-          letters={letters}
-          setLetters={setLetters}
-          selected={selected}
-          setSelected={setSelected}
-          btnClicked={btnClicked}
-          setBtnClicked={setBtnClicked}
-          writedTo={writedTo}
-          setWritedTo={setWritedTo}
-        />} />
-        <Route path="letter/:id" element={<Letter
-          letters={letters}
-          setLetters={setLetters}
-        />} />
+        <Route
+          path="/"
+          element={
+            <Home
+              artistData={artistData}
+              letters={letters}
+              setLetters={setLetters}
+              selected={selected}
+              setSelected={setSelected}
+              btnClicked={btnClicked}
+              setBtnClicked={setBtnClicked}
+              writedTo={writedTo}
+              setWritedTo={setWritedTo}
+            />
+          }
+        />
+        <Route
+          path="letter/:id"
+          element={<Letter letters={letters} setLetters={setLetters} />}
+        />
       </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
 
 export default Router;
